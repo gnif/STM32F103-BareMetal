ea95dd5e61740f274b9fa33dbebb449baf4fcd04
Geoffrey McRae <geoff@hostfission.com>
2020-10-18 20:35:19 +1100

    [gpio] fixed invalid mask

--- src/gpio.c
+++ src/gpio.c
@@ -12,6 +12,6 @@ void gpio_setup(const uint32_t port, const uint8_t pin, const uint32_t cr_mode,
     shift -= 32;
   }
 
-  *reg = (*reg & ~(GPIO_CR_MODE | GPIO_CR_CNF) << shift) |
+  *reg = (*reg & ~((GPIO_CR_MODE | GPIO_CR_CNF) << shift)) |
     (cr_mode | cr_cnf) << shift;
 }
