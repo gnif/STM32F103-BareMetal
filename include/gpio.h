f548097166b1c22464f3f7cadee293a51344cf40
Geoffrey McRae <geoff@hostfission.com>
2020-10-18 16:02:02 +1100

    more gpio cleanup

--- include/gpio.h
+++ include/gpio.h
@@ -8,15 +8,22 @@
 void gpio_setup(const uint32_t port, const uint8_t pin, const uint32_t cr_mode,
     const uint32_t cr_cnf);
 
-#define GPIO_PIN_SET(port, pin) GPIOx_BSRR(port) |= (1 << pin)
-#define GPIO_PIN_CLR(port, pin) GPIOx_BSRR(port) |= (1 << (pin + 16))
+#define GPIO_PORT_WRITE(port, value) GPIOx_ODR(port) = ((value) & 0xFFFF)
+
+#define GPIO_PIN_SET(port, pin) GPIOx_BSRR(port) |= (1 << (pin))
+#define GPIO_PIN_CLR(port, pin) GPIOx_BSRR(port) |= (1 << ((pin) + 16))
+#define GPIO_PIN_SET_GROUP(port, mask) \
+  GPIOx_BSRR(port) |= (((mask) & 0xFFFF) <<  0)
+#define GPIO_PIN_CLR_GROUP(port, mask) \
+  GPIOx_BSRR(port) |= (((mask) & 0xFFFF) << 16)
+
 #define GPIO_PIN_TOG(port, pin) \
   do \
   { \
-    if (GPIOx_ODR(port) & (1 << pin)) \
-      GPIOx_BSRR(port) |= (1 << (pin + 16)); \
+    if (GPIOx_ODR(port) & (1 << (pin))) \
+      GPIO_PIN_CLR(port, pin); \
     else \
-      GPIOx_BSRR(port) |= (1 << pin); \
+      GPIO_PIN_SET(port, pin); \
   } \
   while(0)
 
